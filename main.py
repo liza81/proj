32933b1ca7fcb4e303518b60158f1438c6c0339b
liza81 <lizakalafat@gmail.com>
2024-12-26 02:26:24 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
 from functions import load_titles
 from pprint import pprint
 
@@ -10,17 +8,3 @@ def main():
 
 # Вызов
 main()
-=======
-# main - стартовый модуль проекта
-
-from functions import func1, func2
-
-def main():
-    pass
-
-if __name__ == '__main__':
-    main()
->>>>>>> 3d9d8eb7245fd79141c043ad406cb5de7861466f
-=======
-
->>>>>>> 0b2da87db299c3cab44f7b5857bef7863e674a13
